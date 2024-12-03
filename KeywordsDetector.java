3d155f5bb2c5e654eddc197fc3566ff3c8ec9698
Or Pravda <orpravda@Ors-MacBook-Pro.local>
2024-12-03 22:52:36 +0200

    k

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -24,15 +24,15 @@ public class KeywordsDetector {
         String[] newSentences = new String[sentences.length];
         for(int i=0; i< sentences.length; i++){
             newSentences[i] = sentences[i];
-        }
+            newSentences[i] = (String) MyString.lowerCase(newSentences[i]);
+        } 
+        
         String[] newKeywords = new String[keywords.length];
         for(int i=0; i< keywords.length; i++){
             newKeywords[i] = keywords[i];
+            newKeywords[i] = (String) MyString.lowerCase(newKeywords[i]);
         }
-
-        for(int i=0; i < newSentences.length; i++){
-            newSentences[i] = (String) MyString.lowerCase(newSentences[i]);
-        }
+        
         for(int i=0; i < newSentences.length; i++){
             for(int j=0; j < newKeywords.length; j++){
                 if(MyString.contains(newSentences[i],newKeywords[j])){
@@ -41,6 +41,8 @@ public class KeywordsDetector {
             }   
         }
     }
+    //Expected:%0AOur product will transform the market%0AWe need to leverage our core competencies%0AThis blockchain-based solution will disrupt the industry%0AThe team showed great Synergy in the last project%0AOur new technology presents a significant paradigm shift%0
+    // AActual:%0AOur product will transform the market%0AWe need to leverage our core competencies%0AThis blockchain-based solution will disrupt the industry%0AThe team showed great Synergy in the last project
 
     
 
